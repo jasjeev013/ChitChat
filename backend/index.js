f7d162912325c9dd185c5d93d073c669ef579f52
Jasjeev <jasjeev99@gmail.com>
2024-07-08 14:47:26 +0530

    Intialized Project ChitChat Frontend React Backend Node.js

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const PORT = process.env.port || 5000;
+
+
+const router = require('./router')
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection',(socket) => {
+    console.log('We have a new connection');
+
+    socket.on('disconnect',() => {
+        console.log('User have left');
+    })
+})
+
+app.use(router);
+
+server.listen(PORT,() => {
+    console.log(`ChitChat is Running at ${PORT}`)
+})
